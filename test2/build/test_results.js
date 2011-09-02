413fb03bcc9ac87906baef422606c41ebee52cf4
Nickolas Kenyeres <info@knickmedia.com>
2011-09-02 01:32:46 -0400

    Added second test to test blank manifest

---
+++ test2/build/test_results.js
@@ -0,0 +1,12 @@
+console.log('contents of test/src/bin/src5.js');
+
+console.log('contents of test/src/etc/bin/src4.js');
+
+console.log('contents of test/src/etc/bin/src3.js');
+
+console.log('contents of test/src/src2.js');
+
+console.log('contents of test/src/etc/src3.js');
+
+console.log('contents of test/src/bin/src6.js');
+
---
+++ test2/src/etc/bin/src4.js
@@ -0,0 +1,2 @@
+console.log('contents of test/src/etc/bin/src4.js');
+
